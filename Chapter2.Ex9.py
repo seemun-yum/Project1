648af090ec01cb7b37a1e0d5fd5eb4781160097d
seemun69 <64670498+seemun69@users.noreply.github.com>
2021-01-28 17:13:10 +0800

    Delete Chapter2.Ex9.py

--- Chapter2.Ex9.py
+++
@@ -1,8 +0,0 @@
-spam=input("enter something for spam")
-if spam=='1':
-    print("hello")
-if spam=="2": #elif in this case is more effcient 
-    print("Howdy")
-else:
-    print("Greetings!")
-        

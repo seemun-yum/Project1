733d79bae172a8442f3584d319895ca4f7b9eced
seemun69 <64670498+seemun69@users.noreply.github.com>
2021-01-28 17:09:44 +0800

    Add files via upload

---
+++ Chapter2.Ex9.py
@@ -0,0 +1,8 @@
+spam=input("enter something for spam")
+if spam=='1':
+    print("hello")
+if spam=="2": #elif in this case is more effcient 
+    print("Howdy")
+else:
+    print("Greetings!")
+        
